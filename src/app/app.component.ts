48490566fa09653e8f46406b9d334e42044b22ce
Alexis Daloglou <a.daloglou@public.gr>
2018-12-19 17:14:02 +0200

    tre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('valueInput') valueInput: ElementRef;
+
   title = 'app';
   Arr = Array; // Array type captured in a variable
-  num:number = 10;
+  num: number = 10;
+
+  onPressMe(value: Number) {
+    this.valueInput.nativeElement.value = value;
+  }
 }
